ff96ef1ff0758d3cbc24079e36748365ffe9fd60
Hfuvy <fokoufossojordan@gmail.com>
2024-08-14 03:42:37 +0200

    homepage fin de constrcution de la gallery d'image et stylisation

---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory  from "../../components/menu-item/directory/directory.components";
+import './homepage.style.scss';
+const HomePage = () => (
+  <div className="hompage">
+    <Directory />
+  </div>
+  
+);
+
+export default HomePage;
