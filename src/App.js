04b54a41fcaf3d883f6ff09d10d648940a2b69f1
Hfuvy <fokoufossojordan@gmail.com>
2024-08-16 04:50:46 +0200

    mis a jour du menu item usenavigate a la place de withRouter qui est obselete depuis la version 6 de react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/hats" element={<HatsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
